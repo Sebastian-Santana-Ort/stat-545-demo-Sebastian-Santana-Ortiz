d3b99650d98719f93a0722017419556396be2161
Sebastian Santana Ortiz <santanaort.sebastian@gmail.com>
2023-09-14 09:37:21 -0700

    Second commit from R Studio!

---
+++ Clone Github using R Script.R
@@ -0,0 +1,6 @@
+usethis::create_from_github(
+  "https://github.com/Sebastian-Santana-Ort/stat-545-demo-Sebastian-Santana-Ortiz.git",
+  destdir = "~/OneDrive/2024 - Winter/STAT 545A"
+)
+
+# C:/Users/Sebastian/OneDrive/2024 - Winter/STAT 545A
